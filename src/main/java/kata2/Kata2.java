a629eff0bf2e7c1c8a880aad2e453091b4e5fdd0
danielhdezschez~ <danielersan@gmail.com>
2022-10-05 12:00:21 +0100

    Second commit

--- src/main/java/kata2/Kata2.java
+++ src/main/java/kata2/Kata2.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
 
 package kata2;
 
@@ -12,15 +8,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] data = {1,2,3,3,2,3,5,4,6,6,7,3,8,-4,5};
+        int[] data = {1,2,3,3,2,3,5,4,6,6,7,6,8,-4,5};
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])) {
-                histogram.put(data[i],histogram.get(data[i])+1);
-            } else {
-                histogram.put(data[i], 1);
-            }
-        }
+        for (int key : data) {
+           histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+        }   
+        
         for (Integer key : histogram.keySet()) {
             System.out.println(key + "==>"+ histogram.get(key));
         }
